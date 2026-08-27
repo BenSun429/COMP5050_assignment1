f0ab6da5d33ce2d0b420f4a30327d94cfbef9926
BenSun429 <sunjianghan429@icloud.com>
2026-08-27 21:16:51 +0800

    commit 1
    The data structures needed to define an NFA, along with the NFA construction class, are used to implement regular expression preprocessing.

---
+++ src/State.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ε-NFA State Class
+ */
+public class State {
+
+    public int id;
+    public boolean isAccept;
+
+    // ε transition: List<target state>
+    public List<State> epsilonTransitions = new ArrayList<>();
+
+    // char transition: Map<char, target state>
+    public Map<Character, State> transitions = new HashMap<>();
+
+    public State() {
+        this.isAccept = false;
+    }
+}
